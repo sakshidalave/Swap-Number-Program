f53cf21413a901139e31818e83bd6023b19a672c
sakshidalave <sakshidalave2008@gmail.com>
2026-02-22 19:43:48 +0530

    average.java

---
+++ sumaverg.java
@@ -0,0 +1,16 @@
+class AverageProgram {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        int sum = num1 + num2 + num3;
+        double average = sum / 3.0;
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Third Number: " + num3);
+        System.out.println("Average is: " + average);
+    }
+}
